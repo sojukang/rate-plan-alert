9b56240e5dd1d28f470907ca58bac2b045feff1d
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-09-06 23:14:51 +0900

    set up project

---
+++ src/main/java/com/rateplanalert/RatePlanAlertApplication.java
@@ -0,0 +1,13 @@
+package com.rateplanalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatePlanAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatePlanAlertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rateplanalert/RatePlanAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rateplanalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatePlanAlertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
